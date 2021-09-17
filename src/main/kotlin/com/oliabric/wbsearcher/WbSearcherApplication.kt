bc4f6f74de86ffda0b92e8b5ec592cd4ed6c87f4
OlgaRiabkova <oliabric@gmail.com>
2021-09-17 13:57:17 +0500

    first

---
+++ src/main/kotlin/com/oliabric/wbsearcher/WbSearcherApplication.kt
@@ -0,0 +1,20 @@
+package com.oliabric.wbsearcher
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class WbsearcherApplication
+
+fun main(args: Array<String>) {
+    runApplication<WbsearcherApplication>(*args)
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    fun index() = "Hello"
+}
+
---
+++ src/test/kotlin/com/oliabric/wbsearcher/WbsearcherApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oliabric.wbsearcher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WbsearcherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
